a0518de2874d28dedc8e5f390eb8d1035e241a24
MD-SAZZAD-HOSSEN-272 <md.sazzadhosen325@gmail.com>
2025-02-09 13:02:35 +0600

    added javascript

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const display = document.querySelector('#input')
 
+document.querySelector(".keys").addEventListener("click", e => {
+    if (e.target.value === "=") {
+        try {
+            display.value = eval(display.value);
+        } catch (error) {
+            display.value = "Error"
+        }
+    }
+    else if (e.target.value === "ac") {
+        display.value = "";
+    }
+    else if (e.target.value === "d") {
+        display.value = display.value.slice(0, -1)
+    }
+    else if (e.target.value === "%") {
+        display.value = parseFloat(display.value) / 100;
+    }
+    else {
+        display.value += e.target.value;
+    }
+})
 
-function button (click) {
-    display.value += click;
-}
-function allClear () {
-    display.value = '';
-}
-function calculate () {
-    try {
-        display.value = eval (display.value);
-    }
-    catch (error) {
-        display.value = 'Error';
-    }
-}
+
+// function button (click) {
+//     display.value += click;
+// }
+// function allClear () {
+//     display.value = '';
+// }
+// function calculate () {
+//     try {
+//         display.value = eval (display.value);
+//     }
+//     catch (error) {
+//         display.value = 'Error';
+//     }
+// }
